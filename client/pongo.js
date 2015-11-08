a3063c2ba56fb1515f05e0a27ccb612d3603c348
Bernard Mordan <bernardmordan@gmail.com>
2015-11-08 20:33:32 +0000

    y motion

--- client/pongo.js
+++ client/pongo.js
@@ -1,6 +1,6 @@
-var color = '#'+Math.random().toString(16).substr(-6)
-var updateBatPosition = function (e) { Streamy.emit('update', { data: e.pageX, color: color })}
-var connections = new ReactiveVar(1)
+const color = '#'+Math.random().toString(16).substr(-6)
+const updateBatPosition = function (e) { Streamy.emit('update', { data: e.pageX, color: color })}
+const connections = new ReactiveVar(1)
 
 Streamy.on('connectedUpdate', (connected) => connections.set(connected.data))
 Streamy.on('purge', (id) => $('#'+id.data).remove())
@@ -8,32 +8,33 @@ Streamy.on('motionUpdate', (deltas) => {
   _.each(deltas, (delta) => {
     for (k in delta) {
       var el = $('#'+k)
-      if (!el.length) $('#bat-run').append('<span id="'+k+'">---</span>')
+      if (!el.length) $('#bat-run').append('<span id="'+k+'" class="bats">---</span>')
       el.css('left', delta[k].data+'px').css('color', delta[k].color)
     }   
   })
 })
 
-Template.bat.onRendered( () => document.onmousemove = updateBatPosition)
-Template.connected.onRendered( () => Meteor.call('broadcastConnections'))
-Template.connected.helpers({
-  getConnected: () => connections.get()
-})
+Template.bat.onRendered( () => document.onmousemove = updateBatPosition )
 
-Template.ball.onRendered(() => {
-  var ball = new ui.Actor({
-    element: '#ball',
+Template.connected.onRendered( () => Meteor.call('broadcastConnections') )
+Template.connected.helpers( {getConnected: () => connections.get()} )
+
+Template.ball.onRendered( () => {
+  Streamy.on('ball', (ball) => {
+    $('#ball')
+      .css('left', ball.data.x+'px')
+      .css('top', ball.data.y+'px')
   })
-  var ballPhysics = new ui.Simulate({
-    values: {
-      y: {
-        velocity: 300,
-        acceleration: 300,
-        min: 0,
-        max: window.innerHeight - 140,
-        bounce: 1
-      }
-    }
-  })
-  ball.start(ballPhysics)
+  Meteor.setInterval(() => {
+    let ball = $('#ball').position()
+    let paddles = $('.bats')
+    let bounce = _.some(paddles, function (paddle) {
+      return ball.left > $(paddle).position().left &&
+             ball.left < $(paddle).position().left + 92 &&
+             ball.top > window.innerHeight - 130
+    })
+    if (ball.top < -25) Streamy.emit('top')
+    if (ball.top > window.innerHeight) Streamy.emit('over')
+    if (bounce) Streamy.emit('bounce')
+  }, 16.6666)
 })
--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 connections = {}
+ball = { x:50, y:0, xv:0, yv:1 }
+
+const ballControl = () => {
+  ball.x += ball.xv
+  ball.y += ball.yv
+  Streamy.broadcast('ball', { data: ball })
+}
 
 Meteor.startup(() => {
   connections = {}
   Meteor.setInterval(() => Meteor.call('broadcastLoop'), 16.6666)
+  Meteor.setInterval(() => Meteor.call('ballControl'), 16.6666)
 })
 
 Meteor.methods({
   broadcastConnections: () => Streamy.broadcast('connectedUpdate', { data: _.keys(connections).length}),
   broadcastLoop: () => Streamy.broadcast('motionUpdate', { data: connections }),
-  assignColor: () => _.keys(connections)
+  assignColor: () => _.keys(connections),
+  ballControl: ballControl
 })
 
 Streamy.onConnect( (s) => { 
@@ -21,6 +30,7 @@ Streamy.onDisconnect( (s) => {
   Meteor.call('broadcastConnections')
 })
 
-Streamy.on('update', (data, from) => {
-  connections[from.id] = data
-})
+Streamy.on('update', (data, from) => connections[from.id] = data)
+Streamy.on('bounce', () => ball.yv = -1)
+Streamy.on('top', () => ball.yv = 1)
+Streamy.on('over', () => ball.y = 1)
